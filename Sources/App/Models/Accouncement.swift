e4720854ddf8447dec25065643987f451b9c4946
iDoyoung <ido.young@icloud.com>
2023-04-03 14:25:44 +0900

    Fix To Model For DB

--- Sources/App/Models/Accouncement.swift
+++ Sources/App/Models/Accouncement.swift
@@ -6,10 +6,35 @@
 //
 
 import Vapor
+import Fluent
 
-struct Announcement: Content {
-    let id: UUID
-    let title: String
-    let content: String
-    let date: Date
+final class Announcement: Model {
+    
+    static let schema = "announcements"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    @Field(key: "content")
+    var content: String
+    
+    @Field(key: "date")
+    var date: Date
+    
+    @Timestamp(key: "deleted_at", on: .delete)
+    var deletedAt: Date?
+    
+    init() { }
+    
+    init(id: UUID, title: String, content: String, date: Date) {
+        self.id = id
+        self.title = title
+        self.content = content
+        self.date = date
+    }
 }
+
+extension Announcement: Content { }
