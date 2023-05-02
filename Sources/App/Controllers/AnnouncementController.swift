8f217eed0c5da0b02d8a1cb2050c395b7847f899
iDoyoung <ido.young@icloud.com>
2023-05-02 14:19:31 +0900

    Fix Path Of Create

--- Sources/App/Controllers/AnnouncementController.swift
+++ Sources/App/Controllers/AnnouncementController.swift
@@ -11,25 +11,25 @@ struct AnnouncementController: RouteCollection {
     func boot(routes: Vapor.RoutesBuilder) throws {
         let announcementRoutes = routes.grouped("announcements")
         announcementRoutes.get(use: getAllHandler)
-        announcementRoutes.post("announcement", use: createHandler)
+        announcementRoutes.post("new", use: createHandler)
     }
     
     func getAllHandler(_ req: Request) async throws -> [Announcement] {
         print("Header: \(req.headers)")
         //FIXME: Be Better
-        let id = req.headers["X-Did-Client-Id"].first
-        if id != "TeStHeAdEr" {
-            throw Abort(.nonAuthoritativeInformation)
-        }
+//        let id = req.headers["X-Did-Client-Id"].first
+//        if id != "TeStHeAdEr" {
+//            throw Abort(.nonAuthoritativeInformation)
+//        }
         return try await Announcement.query(on: req.db).all()
     }
     
     func createHandler(_ req: Request) async throws -> Announcement {
         let announcement = try req.content.decode(Announcement.self)
-        let id = req.headers["X-Did-Client-Id"].first
-        if id != "TeStHeAdEr" {
-            throw Abort(.nonAuthoritativeInformation)
-        }
+//        let id = req.headers["X-Did-Client-Id"].first
+//        if id != "TeStHeAdEr" {
+//            throw Abort(.nonAuthoritativeInformation)
+//        }
         try await announcement.save(on: req.db)
         return announcement
     }
