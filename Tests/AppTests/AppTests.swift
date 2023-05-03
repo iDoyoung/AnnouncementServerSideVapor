c24b1505f056096c3aa4295ebdb981f82c2f404b
iDoyoung <ido.young@icloud.com>
2023-05-03 16:57:19 +0900

    Test Create Announcement

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -3,6 +3,8 @@ import XCTVapor
 
 final class AppTests: XCTestCase {
     
+    let mockAnnouncement = Announcement(title: "Test Announcement", content: "Unit Testing...")
+    
     func testAnnouncements() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
@@ -10,6 +12,18 @@ final class AppTests: XCTestCase {
         
         try app.test(.GET, "announcements" , afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
+        })
+    }
+    
+    func test_createAnnouncement() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+        
+        try app.test(.POST, "announcements/new", beforeRequest: { req in
+            try req.content.encode(mockAnnouncement)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .created)
         })
     }
 }
